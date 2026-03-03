ea8a21657dbf3872c72ea289fdd16c5654948b92
rzofchak8 <r.zofchak8@gmail.com>
2026-03-02 19:14:19 -0500

    initial commit

---
+++ discord_connection.py
@@ -0,0 +1,28 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents=discord.Intents(8) 
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
+
+
+create_command = discord.app_commands.Command()
